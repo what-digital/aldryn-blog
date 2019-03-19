bfc1d03d5e7f6fad601030daaa21d0d7a2fbcc8f
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2019-03-19 18:31:09 +0200

    Fix import

--- aldryn_blog/cms_apps.py
+++ aldryn_blog/cms_apps.py
@@ -2,7 +2,7 @@
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
-from aldryn_blog.menu import BlogCategoryMenu
+from aldryn_blog.cms_menus import BlogCategoryMenu
 
 
 class BlogApp(CMSApp):
