ef168428592f9f7f7718777ff32e8d3c99401e2c
dvanser <ivans.dorohovs@gmail.com>
2025-08-07 17:49:20 +0300

    update urls

--- aldryn_blog/urls.py
+++ aldryn_blog/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import re_path
 
 from aldryn_blog.feeds import CategoryFeed, LatestEntriesFeed, TagFeed
 from aldryn_blog.views import (ArchiveView, AuthorEntriesView, AuthorsListView, CategoryListView, CategoryPostListView,
@@ -8,19 +8,19 @@ from aldryn_blog.views import (ArchiveView, AuthorEntriesView, AuthorsListView, 
 app_name = 'aldryn_blog'
 
 urlpatterns = [
-    url(r'^$', ArchiveView.as_view(), name='latest-posts'),
-    url(r'^author/$', AuthorsListView.as_view(), name='author-list'),
-    url(r'^author/(?P<slug>[\w.@+-]+)/$', AuthorEntriesView.as_view(), name='author-posts'),
-    url(r'^feed/$', LatestEntriesFeed(), name='latest-posts-feed'),
-    url(r'^(?P<year>\d{4})/$', ArchiveView.as_view(), name='archive-year'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', ArchiveView.as_view(), name='archive-month'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', ArchiveView.as_view(), name='archive-day'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>\w[-\w]*)/$',
+    re_path(r'^$', ArchiveView.as_view(), name='latest-posts'),
+    re_path(r'^author/$', AuthorsListView.as_view(), name='author-list'),
+    re_path(r'^author/(?P<slug>[\w.@+-]+)/$', AuthorEntriesView.as_view(), name='author-posts'),
+    re_path(r'^feed/$', LatestEntriesFeed(), name='latest-posts-feed'),
+    re_path(r'^(?P<year>\d{4})/$', ArchiveView.as_view(), name='archive-year'),
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', ArchiveView.as_view(), name='archive-month'),
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', ArchiveView.as_view(), name='archive-day'),
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>\w[-\w]*)/$',
         PostDetailView.as_view(), name='post-detail'),
-    url(r'^category/$', CategoryListView.as_view(), name='category-list'),
-    url(r'^category/(?P<category>[-\w]+)/$', CategoryPostListView.as_view(), name='category-posts'),
-    url(r'^category/(?P<category>[-\w]+)/feed/$', CategoryFeed(), name='category-posts-feed'),
-    url(r'^tag/$', TagsListView.as_view(), name='tag-list'),
-    url(r'^tag/(?P<tag>[-\w]+)/$', TaggedListView.as_view(), name='tagged-posts'),
-    url(r'^tag/(?P<tag>[-\w]+)/feed/$', TagFeed(), name='tagged-posts-feed'),
+    re_path(r'^category/$', CategoryListView.as_view(), name='category-list'),
+    re_path(r'^category/(?P<category>[-\w]+)/$', CategoryPostListView.as_view(), name='category-posts'),
+    re_path(r'^category/(?P<category>[-\w]+)/feed/$', CategoryFeed(), name='category-posts-feed'),
+    re_path(r'^tag/$', TagsListView.as_view(), name='tag-list'),
+    re_path(r'^tag/(?P<tag>[-\w]+)/$', TaggedListView.as_view(), name='tagged-posts'),
+    re_path(r'^tag/(?P<tag>[-\w]+)/feed/$', TagFeed(), name='tagged-posts-feed'),
 ]
