574516ba005734b198f2578f95fd0f5f4a2d30d3
dvanser <ivans.dorohovs@gmail.com>
2025-08-07 18:36:27 +0300

    update paginator

--- aldryn_blog/views.py
+++ aldryn_blog/views.py
@@ -17,7 +17,7 @@ from django.views.generic.list import ListView
 from django.contrib.auth.models import User
 
 from menus.utils import set_language_changer
-from aldryn_common.paginator import DiggPaginator
+from django.core.paginator import Paginator
 
 from aldryn_blog import request_post_identifier
 from .models import Post, Category
@@ -43,7 +43,6 @@ class BasePostView(object):
 
 
 class ArchiveView(BasePostView, ArchiveIndexView):
-
     date_field = 'publication_start'
     allow_empty = True
     allow_future = True
@@ -59,18 +58,21 @@ class ArchiveView(BasePostView, ArchiveIndexView):
         return qs
 
     def get_context_data(self, **kwargs):
-        # Make sure page request is an int. If not, deliver first page.
         try:
             page = int(self.request.GET.get('page', 1))
         except ValueError:
             page = 1
+        
         kwargs['day'] = int(self.kwargs.get('day')) if 'day' in self.kwargs else None
         kwargs['month'] = int(self.kwargs.get('month')) if 'month' in self.kwargs else None
         kwargs['year'] = int(self.kwargs.get('year')) if 'year' in self.kwargs else None
         if kwargs['year']:
             kwargs['archive_date'] = datetime.date(
                 kwargs['year'], kwargs['month'] or 1, kwargs['day'] or 1)
-        kwargs['page'] = DiggPaginator(kwargs['object_list'], paginate_by(), body=6, padding=2).page(page)
+        
+        # Use Django's Paginator
+        paginator = Paginator(kwargs['object_list'], paginate_by())  # replace DiggPaginator
+        kwargs['page'] = paginator.get_page(page)
         kwargs['object_list'] = kwargs['page'].object_list
         return super(ArchiveView, self).get_context_data(**kwargs)
 
